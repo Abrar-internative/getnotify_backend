3f86c19c2ac3948303f199ca2a0e166ad9fd9c23
Abrar <abrar123789ali@gmail.com>
2023-05-08 14:10:24 +0500

    ok

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,39 +1,46 @@
 const fs = require('fs');
 
-function jsonReader(filePath, cb) {
-    fs.readFile(filePath, (err, fileData) => {
-      if (err) {
-        return cb && cb(err);
-      }
-      try {
-        const object = JSON.parse(fileData);
-        return cb && cb(null, object);
-      } catch (err) {
-        return cb && cb(err);
-      }
-    });
-  }
+// function jsonReader(filePath, cb) {
+//     fs.readFile(filePath, (err, fileData) => {
+//       if (err) {
+//         return cb && cb(err);
+//       }
+//       try {
+//         const object = JSON.parse(fileData);
+//         return cb && cb(null, object);
+//       } catch (err) {
+//         return cb && cb(err);
+//       }
+//     });
+//   }
 
 const preferences = async (req, res) => {
-    const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
+  const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
 
-    const preferences = {
-        mode: mode || false,
-        read: read ? true : false,
-        link: link || false,
-        attachment: attachment || false,
-        send: send || 'individually',
-        notification: notification || 'sms',
-        readCount: readCount || 3,
-        linkCount: linkCount || 2,
-    }
-    
-    jsonReader('./data/account.txt', (err,result)=>{
-        fs.writeFile('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
-            if(err) return res.status(500)
-            res.status(200).send('updated successfully')
-        })
-    })
+  const preferences = {
+      mode: mode || false,
+      read: read ? true : false,
+      link: link || false,
+      attachment: attachment || false,
+      send: send || 'individually',
+      notification: notification || 'sms',
+      readCount: readCount || 3,
+      linkCount: linkCount || 2,
+  }
+  await fs.open('./data/account.txt','r+',async function(err,fd){
+    // console.log(fd)
+    await fs.readFile('./data/account.txt',(err,data)=>{
+        //  console.log(JSON.parse(data))
+         const result = JSON.parse(data)
+         fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
+                if(err) return res.status(500)
+                
+                return res.status(200).send('updated successfully')
+                
+            })
+       })
+    return res.status(200).send('updated successfully')
+  })
     
 }
 const userInfo = async (req, res) => {
@@ -55,11 +62,19 @@ const userInfo = async (req, res) => {
   currency: currency,
   }
   
-  jsonReader('./data/account.txt', (err,result)=>{
-      fs.writeFile('./data/account.txt',JSON.stringify({...result,general},null,2),(err,data)=>{
-          if(err) return res.status(500)
-          res.status(200).send('updated successfully')
-      })
+  await fs.open('./data/account.txt','r+',async function(err,fd){
+    // console.log(fd)
+    await fs.readFile('./data/account.txt',(err,data)=>{
+        //  console.log(JSON.parse(data))
+         const result = JSON.parse(data)
+         fs.writeFileSync('./data/account.txt',JSON.stringify({...result,general},null,2),(err,data)=>{
+                if(err) return res.status(500)
+                
+                return res.status(200).send('updated successfully')
+                
+            })
+       })
+    return res.status(200).send('updated successfully')
   })
   
 }
--- index.js
+++ index.js
@@ -27,73 +27,73 @@ io.on('connection',socket=>{
 )
 httpServer.listen(5500)
 
- function jsonReader(filePath, cb) {
-  fs.readFileSync(filePath, (err, fileData) => {
-    if (err) {
-      return cb && cb(err);
-    }
-    try {
-      const object = JSON.parse(fileData);
-      return cb && cb(null, object);
-    } catch (err) {
-      return cb && cb(err);
-    }
-  });
-}
-const data = {
-    general: {
-    //   avatar: avatar11, 
-      username: 'johndoe',
-      fullName: 'John Doe',
-      email: 'granger007@hogward.com',
-      company: 'PIXINVENT'
-    },
-    info: {
-      bio: '',
-      dob: null,
-      country: 'USA',
-      website: '',
-      phone: 6562542568
-    },
-    social: {
-      socialLinks: {
-        twitter: 'https://www.twitter.com',
-        facebook: '',
-        google: '',
-        linkedIn: 'https://www.linkedin.com',
-        instagram: '',
-        quora: ''
-      },
-      connections: {
-        twitter: {
-        //   profileImg: avatar11,
-          id: 'johndoe'
-        },
-        google: {
-        //   profileImg: avatar3,
-          id: 'luraweber'
-        },
-        facebook: {},
-        github: {}
-      }
-    },
-    notification: {
-      commentOnArticle: true,
-      answerOnForm: true,
-      followMe: false,
-      newAnnouncements: true,
-      productUpdates: true,
-      blogDigest: false 
-    },
-    preferences:{
-      mode: true,
-      events:[{read:true,link:false,attachment:true}],
-      send: 'individually',
-      notification: 'sms',
-      emailReadCount: 3,
-      linkClickedCount: 2,
-    }
-  }
+//  function jsonReader(filePath, cb) {
+//   fs.readFileSync(filePath, (err, fileData) => {
+//     if (err) {
+//       return cb && cb(err);
+//     }
+//     try {
+//       const object = JSON.parse(fileData);
+//       return cb && cb(null, object);
+//     } catch (err) {
+//       return cb && cb(err);
+//     }
+//   });
+// }
+// const data = {
+//     general: {
+//     //   avatar: avatar11, 
+//       username: 'johndoe',
+//       fullName: 'John Doe',
+//       email: 'granger007@hogward.com',
+//       company: 'PIXINVENT'
+//     },
+//     info: {
+//       bio: '',
+//       dob: null,
+//       country: 'USA',
+//       website: '',
+//       phone: 6562542568
+//     },
+//     social: {
+//       socialLinks: {
+//         twitter: 'https://www.twitter.com',
+//         facebook: '',
+//         google: '',
+//         linkedIn: 'https://www.linkedin.com',
+//         instagram: '',
+//         quora: ''
+//       },
+//       connections: {
+//         twitter: {
+//         //   profileImg: avatar11,
+//           id: 'johndoe'
+//         },
+//         google: {
+//         //   profileImg: avatar3,
+//           id: 'luraweber'
+//         },
+//         facebook: {},
+//         github: {}
+//       }
+//     },
+//     notification: {
+//       commentOnArticle: true,
+//       answerOnForm: true,
+//       followMe: false,
+//       newAnnouncements: true,
+//       productUpdates: true,
+//       blogDigest: false 
+//     },
+//     preferences:{
+//       mode: true,
+//       events:[{read:true,link:false,attachment:true}],
+//       send: 'individually',
+//       notification: 'sms',
+//       emailReadCount: 3,
+//       linkClickedCount: 2,
+//     }
+//   }
 app.use(logger) ;//log requests to console and logfile 
 
 app.use(express.urlencoded({ extended: true }));
@@ -120,61 +120,61 @@ app.use(function(req, res, next) {
   next();
 });
 // app.options('*',cors())
-app.get('/noti',(req,res)=>{
-  res.status(200).send("gotchya")
-})
-app.post('/noti',(req,res)=>{
-  res.status(200).send("gotchya again")
-})
-app.post('/test', async (req,res)=>{
-  // console.log(req.body)
-  const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
+// app.get('/noti',(req,res)=>{
+//   res.status(200).send("gotchya")
+// })
+// app.post('/noti',(req,res)=>{
+//   res.status(200).send("gotchya again")
+// })
+// app.post('/test', async (req,res)=>{
+//   // console.log(req.body)
+//   const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
 
-    const preferences = {
-        mode: mode || false,
-        read: read ? true : false,
-        link: link || false,
-        attachment: attachment || false,
-        send: send || 'individually',
-        notification: notification || 'sms',
-        readCount: readCount || 3,
-        linkCount: linkCount || 2,
-    }
-    await fs.open('./data/account.txt','r+',async function(err,fd){
-      console.log(fd)
-      await fs.readFile('./data/account.txt',(err,data)=>{
-           console.log(JSON.parse(data))
-           const result = JSON.parse(data)
-           fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
-                  if(err) return res.status(500)
+//     const preferences = {
+//         mode: mode || false,
+//         read: read ? true : false,
+//         link: link || false,
+//         attachment: attachment || false,
+//         send: send || 'individually',
+//         notification: notification || 'sms',
+//         readCount: readCount || 3,
+//         linkCount: linkCount || 2,
+//     }
+//     await fs.open('./data/account.txt','r+',async function(err,fd){
+//       console.log(fd)
+//       await fs.readFile('./data/account.txt',(err,data)=>{
+//            console.log(JSON.parse(data))
+//            const result = JSON.parse(data)
+//            fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
+//                   if(err) return res.status(500)
                   
-                  return res.status(200).send('updated successfully')
+//                   return res.status(200).send('updated successfully')
                   
-              })
-         })
-      return res.status(200).send('updated successfully')
-    })
-    //  await fs.readFile('./data/account.txt',(err,data)=>{
-    //    console.log(JSON.parse(data))
-    //    const result = JSON.parse(data)
-    //    fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
-    //           if(err) return res.status(500)
+//               })
+//          })
+//       return res.status(200).send('updated successfully')
+//     })
+//     //  await fs.readFile('./data/account.txt',(err,data)=>{
+//     //    console.log(JSON.parse(data))
+//     //    const result = JSON.parse(data)
+//     //    fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
+//     //           if(err) return res.status(500)
               
-    //           return res.status(200).send('updated successfully')
+//     //           return res.status(200).send('updated successfully')
               
-    //       })
-    //  }) 
+//     //       })
+//     //  }) 
     
-    // jsonReader('./data/account.txt', (err,result)=>{
-    //   if(err) return res.status(500).send(err.message)
-    //   fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
-    //       if(err) return res.status(500)
+//     // jsonReader('./data/account.txt', (err,result)=>{
+//     //   if(err) return res.status(500).send(err.message)
+//     //   fs.writeFileSync('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
+//     //       if(err) return res.status(500)
           
-    //       return res.status(200).send('updated successfully')
+//     //       return res.status(200).send('updated successfully')
           
-    //   })
-    // })
-})
+//     //   })
+//     // })
+// })
 
 app.use('/update', require('./routes/update'));
 app.get('/account', (req, res) => {
