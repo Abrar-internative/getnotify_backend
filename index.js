f33f6474a37db55a2274e980d4b85cfb8ecc37a0
Abrar <abrar123789ali@gmail.com>
2023-05-08 13:16:09 +0500

    testing

--- index.js
+++ index.js
@@ -127,27 +127,28 @@ app.post('/noti',(req,res)=>{
   res.status(200).send("gotchya again")
 })
 app.post('/test',(req,res)=>{
-  const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
+  console.log(req.body)
+  // const {mode,read,link,attachment,send,notification,readCount,linkCount} = req.body
 
-    const preferences = {
-        mode: mode || false,
-        read: read ? true : false,
-        link: link || false,
-        attachment: attachment || false,
-        send: send || 'individually',
-        notification: notification || 'sms',
-        readCount: readCount || 3,
-        linkCount: linkCount || 2,
-    }
+  //   const preferences = {
+  //       mode: mode || false,
+  //       read: read ? true : false,
+  //       link: link || false,
+  //       attachment: attachment || false,
+  //       send: send || 'individually',
+  //       notification: notification || 'sms',
+  //       readCount: readCount || 3,
+  //       linkCount: linkCount || 2,
+  //   }
     
-    jsonReader('./data/account.txt', (err,result)=>{
-        fs.writeFile('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
-            if(err) return res.status(500)
+  //   jsonReader('./data/account.txt', (err,result)=>{
+  //       fs.writeFile('./data/account.txt',JSON.stringify({...result,preferences},null,2),(err,data)=>{
+  //           if(err) return res.status(500)
             
-            return res.status(200).send('updated successfully')
+  //           return res.status(200).send('updated successfully')
             
-        })
-    })
+  //       })
+  //   })
 })
 
 app.use('/update', require('./routes/update'));
